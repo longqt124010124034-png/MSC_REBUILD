0bf371b9226a0745cf0d0cc615f6f0680a6e9494
Quách Thành Long <stephensouth1307@gmail.com>
2025-08-04 16:31:41 +0700

    -update

--- components/sections/MentorsSection.tsx
+++ components/sections/MentorsSection.tsx
@@ -85,7 +85,7 @@ const MentorsSection = () => {
         </motion.div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12">
-          {mentors.map((mentor, index) => (
+          {mentors.map((mentor: any, index) => (
             <motion.div
               key={mentor.id}
               initial={{ opacity: 0, y: 50 }}
